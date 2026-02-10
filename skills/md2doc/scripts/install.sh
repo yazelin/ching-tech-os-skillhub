b62d892fe772fccd3de29cc3dc4487104e743092
Yaze Lin <yaze.lin.j303@gmail.com>
2026-02-10 15:14:00 +0800

    chore: remove md2doc/md2ppt skills (incorrect content, not CLI tools)

--- skills/md2doc/scripts/install.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env sh
-# Install md2doc-evolution globally using npm
-npm install -g md2doc-evolution
--- skills/md2ppt/scripts/install.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env sh
-# Install md2ppt-evolution globally using npm
-npm install -g md2ppt-evolution
